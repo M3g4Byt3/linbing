e6a8b28d35bf3b4df953a3bea7b0bbb9f16832f5
taomujian <47107775+taomujian@users.noreply.github.com>
2020-07-09 10:22:46 +0800

    Update run.py

--- flask/run.py
+++ flask/run.py
@@ -1004,5 +1004,5 @@ def change_avatar():
         return str(response_data)
 
 if __name__ == '__main__':
-    app.run(debug = True, port= 5000)
-    #app.run(host='0.0.0.0', port= 8000)
+    #app.run(debug = True, port= 5000)
+    app.run(host='0.0.0.0', port= 8000)
